6af532ce110701facdbc675d1631312228f91c66
anavitoriaq <anavitoriaqueiroz78@gmail.com>
2025-02-28 13:50:12 -0300

    feat: adicionando lista

--- lista-laboratorio/questao1.c
+++ lista-laboratorio/questao1.c
@@ -4,14 +4,11 @@ int main() {
     int num1, num2, num3;
     float media;
 
-    // Solicita os números ao usuário
     printf("Digite tres numeros inteiros: ");
     scanf("%d %d %d", &num1, &num2, &num3);
 
-    // Calcula a média
     media = (num1 + num2 + num3) / 3.0;
 
-    // Exibe o resultado com uma casa decimal
     printf("A media e %.1f.\n", media);
 
     return 0;
